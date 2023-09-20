a1e43bc0fe33f32adfa665123a74aa92a9bb2d22
Todd S. Sorensen <you@example.com>
2023-09-20 11:58:19 -0500

    Set up Projects

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'home/index'
   get 'about', to: 'home#about'
+  get 'dashboard', to:'projects#index'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'home#dashboard'
+  root 'home#home'
 end
